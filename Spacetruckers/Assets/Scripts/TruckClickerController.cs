d109befd8dbf6ce25116bbe3ffb60ee54cc9e113
noskro <matthiasluedemann@googlemail.com>
2021-04-02 18:39:10 +0200

    Detection of Planets - missing parts :)

--- Spacetruckers/Assets/Scripts/TruckClickerController.cs
+++ Spacetruckers/Assets/Scripts/TruckClickerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,17 +26,12 @@ public class TruckClickerController : MonoBehaviour
         if (Input.GetMouseButtonDown(0)) // if left button pressed...
         {
             Debug.Log("Left Click");
+            // if selection a detsination is active, select the next click as destination
             if (this.isSelectDestinationActive)
             {
-                // if selection a detsination is active, select the next click as detsination
+                Vector3 targetLocation = this.getMouseTargetLocation();
 
-                // make a plane at Y level of truck to get click location on that plane
-                Plane planeTruck = new Plane(Vector3.up, this.truck.position.y);
-
-                Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
-
-                float distance;
-                if (planeTruck.Raycast(ray, out distance))
+                if (targetLocation != Vector3.zero)
                 {
                     // set truck target to the click location on the plane of the truck
                     //TODO this should be a prefab
@@ -46,7 +42,7 @@ public class TruckClickerController : MonoBehaviour
                     stopCollider.height = 10f;
                     stopCollider.isTrigger = true;
 
-                    target.transform.position = ray.GetPoint(distance);
+                    target.transform.position = targetLocation;
                     GetComponent<Truck>().target = target.transform;
                 }
 
@@ -60,18 +56,59 @@ public class TruckClickerController : MonoBehaviour
                 // TODO: Close Popup if not clicked in popup
             }
             // else select a detsination is not active, do nothing for now 
-
-
-
-            // to get the object that was clicked on in 3D
-            //Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
-            //RaycastHit hit;
-            //if (Physics.Raycast(ray, out hit))
-            //{
-            //    // the object identified by hit.transform was clicked
-            //    // do whatever you want
-            //}
         }
+        else // if there was no click
+        {
+            // check if mouse hovers over a planet to highlight it
+            Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                // if a planet was clicked
+                if (hit.transform.gameObject.CompareTag("Planet"))
+                {
+                    Debug.Log("highlight Planet: " + hit.transform.gameObject);
+                    // TODO: Highlight game object
+                }
+                else if (hit.transform.gameObject.CompareTag("Truck"))
+                {
+                    Debug.Log("highlight Truck: " + hit.transform.gameObject);
+                    // TODO: Highlight game object
+                }
+            }
+        }
+    }
+
+    private Vector3 getMouseTargetLocation()
+    {
+        Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        // first check if planet was clicked
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            // if a planet was clicked
+            if (hit.transform.gameObject.CompareTag("Planet"))
+            {
+                Debug.Log(hit.transform.gameObject);
+                return new Vector3(hit.transform.position.x, this.truck.position.y, hit.transform.position.z);
+            }
+        }
+
+
+
+        // make a plane at Y level of truck to get click location on that plane
+        Plane planeTruck = new Plane(Vector3.up, this.truck.position.y);
+
+        float distance;
+        if (planeTruck.Raycast(ray, out distance))
+        {
+            return ray.GetPoint(distance);
+        }
+
+
+        return Vector3.zero; 
     }
 
     public void OnMouseDown()
