13dc4c646d02089955a9be08b24e54cf7572b0ef
noskro <matthiasluedemann@googlemail.com>
2021-04-02 11:23:38 +0200

    Enum für Waren zum transportieren

---
+++ Spacetruckers/Assets/Scripts/EnumGoods.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnumGoods
+{
+    _Nichts, 
+    LandLuft,
+    Vakuum,
+    Ostereier,
+    Zuckerstangen,
+    TeddyBaeren,
+    LegoSteine
+}
