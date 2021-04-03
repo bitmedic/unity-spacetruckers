a09de4913528b3e09f53212d4a4ee1004418358d
noskro <matthiasluedemann@googlemail.com>
2021-04-03 14:05:10 +0200

    Truck Popup Destination selection rework

--- Spacetruckers/Assets/Scripts/TruckClickerController.cs
+++ Spacetruckers/Assets/Scripts/TruckClickerController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class TruckClickerController : MonoBehaviour
@@ -11,7 +12,7 @@ public class TruckClickerController : MonoBehaviour
 
     private Transform truck;
     private Button loadCargoAndSelectDestinationButton;
-    private bool isSelectDestinationPopupOpen = false;
+    private bool isSelectCargoPopupOpen = false;
     private bool isSelectDestinationActive = false;
     private Vector3 truckDestination;
 
@@ -30,116 +31,99 @@ public class TruckClickerController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0)) // if left button pressed...
         {
-            Debug.Log("Left Click");
-            // if selection a detsination is active, select the next click as destination
-            if (this.isSelectDestinationActive)
+            if (!EventSystem.current.IsPointerOverGameObject())
             {
-                Vector3 targetLocation = this.getMouseTargetLocation();
-
-                if (targetLocation != Vector3.zero)
-                {
-                    // set truck target to the click location on the plane of the truck
-
-                    this.truck.gameObject.GetComponent<Truck>().LoadCargo(this.toLoadCargo);
-
-                    //TODO this should be a prefab
-                    GameObject target = new GameObject();
-                    target.layer = LayerMask.NameToLayer("Stations");
-                    CapsuleCollider stopCollider = target.AddComponent<CapsuleCollider>();
-                    stopCollider.radius = 0.1f;
-                    stopCollider.height = 10f;
-                    stopCollider.isTrigger = true;
-
-                    target.transform.position = targetLocation;
-                    GetComponent<Truck>().target = target.transform;
-                }
-
-                this.setDestinationSelectionPopupActive(false);
-                this.isSelectDestinationActive = false;
-            }
-            else if (this.isSelectDestinationPopupOpen == true)
-            {
-                //this.setDestinationSelectionPopup(false);
-
-                // TODO: Close Popup if not clicked in popup
-            }
-            // else select a detsination is not active, do nothing for now 
-        }
-        else // if there was no click
-        {
-            // check if mouse hovers over a planet to highlight it
-            Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
-
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                // if a planet was clicked
-                if (hit.transform.gameObject.CompareTag("Planet"))
-                {
-                    Debug.Log("highlight Planet: " + hit.transform.gameObject);
-                    // TODO: Highlight game object
-                }
-                else if (hit.transform.gameObject.CompareTag("Truck"))
-                {
-                    Debug.Log("highlight Truck: " + hit.transform.gameObject);
-                    // TODO: Highlight game object
-                }
+                this.OnMouseClick();
             }
         }
     }
 
-    public void OnMouseDown()
+    public void OnMouseClick()
     {
-        if (truck != null)
+        Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        // first check if planet was clicked
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
         {
-            loadCargoAndSelectDestinationButton.gameObject.SetActive(false);
-
-            // locate the popup for destination selection at the trucks position (the topleft corner at click position)
-            Transform selectDestinationTransform = this.selectDestinationCanvas.GetComponent<Transform>();
-            selectDestinationTransform.position = new Vector3(this.truck.position.x  /*+ (selectDestinationTransform.size.x / 2)*/, 
-                this.truck.position.y + 1, 
-                this.truck.position.z /*+ (selectDestinationTransform.size.z / 2)*/);
-
-            // Get Planet the truck is on (if any)
-            // TODO Get Ray to check from truck position Ray ray = this.mainCamera.ray(this.truck.position);
-            Ray ray = this.mainCamera.ScreenPointToRay(Input.mousePosition); // temporary workaround: the click on the truck is porbably above a planet when cargo is available
-            GameObject planet = this.getPlanetAtLocation(ray);
-            if (planet != null)
+            // if the truck was clicked on the popup is not already open
+            if (hit.transform.gameObject.CompareTag("Truck"))
             {
-                PlanetCargoController pcc = planet.GetComponent<PlanetCargoController>();
-                if (!pcc.producedGoods.Equals(EnumCargo._Nichts)) // if anything gets produced
+                if (this.isSelectCargoPopupOpen) // if popup is already open do nothing
                 {
-                    this.toLoadCargo = pcc.producedGoods;
-                    loadCargoAndSelectDestinationButton.GetComponentInChildren<Text>().text = "Load " + pcc.producedGoods + " & \nSelectdestination";
-                    loadCargoAndSelectDestinationButton.gameObject.SetActive(true);
+                    return;
                 }
+
+                loadCargoAndSelectDestinationButton.gameObject.SetActive(false);
+
+                // locate the popup for destination selection at the trucks position (the topleft corner at click position)
+                RectTransform selectDestinationTransform = this.selectDestinationCanvas.GetComponent<RectTransform>();
+                selectDestinationTransform.position = new Vector3(this.truck.position.x,
+                    this.truck.position.y + 1,
+                    this.truck.position.z - 1f);
+
+                // Get Planet the truck is on (if any)
+                // TODO Get Ray to check from truck position Ray ray = this.mainCamera.ray(this.truck.position);
+                GameObject planet = this.getPlanetAtLocation(ray);
+                if (planet != null)
+                {
+                    PlanetCargoController pcc = planet.GetComponent<PlanetCargoController>();
+                    if (!pcc.producedGoods.Equals(EnumCargo._Nichts)) // if anything gets produced
+                    {
+                        this.toLoadCargo = pcc.producedGoods;
+                        loadCargoAndSelectDestinationButton.GetComponentInChildren<Text>().text = "Load " + pcc.producedGoods;
+                        loadCargoAndSelectDestinationButton.gameObject.SetActive(true);
+
+                        this.setDestinationSelectionPopupActive(true);
+                    }
+
+                }
+
+                this.isSelectDestinationActive = true; // if no cargo is available only select destination and don't show popup
+                return; // if truck was clicked this was it. For all other cases (planet or nothing was clicked) continue
+            }
+        }
+          
+        // if selection a destination is active, select the next click as destination
+        if (this.isSelectDestinationActive)
+        {
+            Vector3 targetLocation = this.getMouseTargetLocation();
+
+            if (targetLocation != Vector3.zero)
+            {
+                //TODO this should be a prefab
+                GameObject target = new GameObject();
+                target.layer = LayerMask.NameToLayer("Stations");
+                CapsuleCollider stopCollider = target.AddComponent<CapsuleCollider>();
+                stopCollider.radius = 0.1f;
+                stopCollider.height = 10f;
+                stopCollider.isTrigger = true;
+
+                target.transform.position = targetLocation;
+                GetComponent<Truck>().target = target.transform;
             }
 
-            this.setDestinationSelectionPopupActive(true);
-        }
-    }
-
-
-    public void SelectDetsinationButtonClick()
-    {
-        this.setDestinationSelectionPopupActive(false);
-
-        // activate select destination so the next click will be set as destination
-        this.isSelectDestinationActive = true; 
-    }
+            this.setDestinationSelectionPopupActive(false);
+            this.isSelectDestinationActive = false;
+        }        
+    } 
 
     public void LoadCargoAndSelectDestinationButtonClick()
     {
+        // load Cargo
+        this.truck.gameObject.GetComponent<Truck>().LoadCargo(this.toLoadCargo);
+
         this.setDestinationSelectionPopupActive(false);
 
-        // activate select destination so the next click will be set as destination
+        // activate select destination so the next click will still be set as destination
         this.isSelectDestinationActive = true;
     }
 
     internal void setDestinationSelectionPopupActive(bool active)
     {
         this.selectDestinationCanvas.gameObject.SetActive(active);
-        this.isSelectDestinationPopupOpen = active;
+        this.isSelectCargoPopupOpen = active;
+        this.isSelectDestinationActive = active;
     }
 
     private Vector3 getMouseTargetLocation()
@@ -158,7 +142,6 @@ public class TruckClickerController : MonoBehaviour
             }
         }
 
-
         // make a plane at Y level of truck to get click location on that plane
         Plane planeTruck = new Plane(Vector3.up, this.truck.position.y);
 
@@ -167,7 +150,6 @@ public class TruckClickerController : MonoBehaviour
         {
             return ray.GetPoint(distance);
         }
-
 
         return Vector3.zero;
     }
